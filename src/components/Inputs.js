cfd79ca18122b636a89a3fc833b91b71358f0449
Gandhamathan V <gandhasaroja@gmail.com>
2023-03-24 11:19:15 +0530

    sdsd

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Inputs(props) {
+export function Inputs(props) {
   return (
     <div class="form-group">
       <label for="exampleInputEmail1">{props.label}</label>
@@ -11,6 +11,19 @@ function Inputs(props) {
         aria-describedby="emailHelp"
         placeholder={props.placeholder}
       />
+    </div>
+  );
+}
+
+export function Selects(props) {
+  return (
+    <div class="form-group">
+      <label for="exampleFormControlSelect1">{props.label}</label>
+      <select class="form-control" id="exampleFormControlSelect1">
+        {props.option.map((el) => (
+          <option>{el}</option>
+        ))}
+      </select>
     </div>
   );
 }
