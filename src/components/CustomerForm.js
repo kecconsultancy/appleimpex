2c09b09f01b685ac582dbaec71b68a7896ff9047
aswathi-sivasubramanian <skaswathisk@gmail.com>
2023-03-24 12:23:18 +0530

    2 files

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -8,12 +8,61 @@ export function CustomerForm() {
           <div class="col-sm-12">
             <div class="card">
               <div class="card-header">
-                <h5>Create Fabric</h5>
+                <h5>Create Customer</h5>
               </div>
               <div class="card-body">
                 <div class="row">
                   <div class="col-md-6">
-                    <form></form>
+                    <form>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Customer Name</label>
+                        <input
+                          type="Customer Name"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="Customer Name"
+                        />
+                        {/* <small id="emailHelp" class="form-text text-muted">
+                          We'll never share your email with anyone else.
+                        </small> */}
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Shop Name</label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="Enter  Shop Nmae"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">
+                          Customer Phone Number
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="Enter  Phone Number"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Address</label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="Enter Address"
+                        />
+                      </div>
+                      <button type="submit" class="btn btn-primary">
+                        Submit
+                      </button>
+                    </form>
                   </div>
                 </div>
               </div>
